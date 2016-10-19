a2191b851c1c2295701aa49e223662e4d81c5aa4
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-10-19 23:19:39 +0530

    Created navbar, footer, home templates with some CSS

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,9 @@ import { Template } from 'meteor/templating';
 
 import './body.html';
 import '../startups/routers';
+import './navbar';
+import './home';
+import './footer';
 
 Template.home.helpers({
     title() {
